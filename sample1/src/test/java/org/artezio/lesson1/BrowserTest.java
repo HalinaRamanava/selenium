c6de9f7e1dc77aa384d19c10087807b13efa0280
HalinaRamanava <galinagcherkasova@gmail.com>
2018-02-08 17:34:05 +0300

    First commit

---
+++ sample1/src/test/java/org/artezio/lesson1/BrowserTest.java
@@ -0,0 +1,36 @@
+package org.artezio.lesson1;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class BrowserTest {
+
+    private Properties properties = new Properties();
+    private WebDriver chromeDriver;
+
+    public static final String PROP_FILE = "/drivers.properties";
+
+    @Before
+    public void init() throws IOException {
+        properties.load(BrowserTest.class.getResourceAsStream(PROP_FILE));
+        properties.stringPropertyNames().forEach(propName -> System.setProperty(propName, properties.getProperty(propName)));
+        chromeDriver = new ChromeDriver();
+    }
+
+    @Test
+    public void openBrowserTest() {
+        chromeDriver.navigate().to("http://www.google.com");
+    }
+
+    @After
+    public void finish() {
+        chromeDriver.quit();
+        chromeDriver = null;
+    }
+}
